e1193d6bed50b34ce4493f6fbbddcda4281e2b7b
Elliot <elliotholbrow@gmail.com>
2019-08-22 13:12:59 +0100

    Clean code

--- slides.js
+++ slides.js
@@ -1,45 +1,45 @@
-// pick all of the images and layer them based on 
+// pick all of the images and layer them based on
 // the z-index
-const slideArea = document.querySelector("div.slides")
-const images = slideArea.querySelectorAll("img")
+const slideArea = document.querySelector("div.slides");
+const images = slideArea.querySelectorAll("img");
 
 // keep track of two things
-let currentSlide = 0
-let z = 1
+let currentSlide = 0;
+let z = 1;
 
 // when i click the slide area, change the slide based on z-index
-slideArea.addEventListener("click", function (){
-    currentSlide = currentSlide + 1
+slideArea.addEventListener("click", function() {
+  currentSlide = currentSlide + 1;
 
-    if (currentSlide > images.length - 1) {
-        currentSlide = 0
-    }
+  if (currentSlide > images.length - 1) {
+    currentSlide = 0;
+  }
 
-    z = z + 1
+  z = z + 1;
 
-    // remove the animation from the style for EVERY IMAGE
-    images.forEach(image => {
-        image.style.animation = ""
-    })
+  // remove the animation from the style for EVERY IMAGE
+  images.forEach(image => {
+    image.style.animation = "";
+  });
 
-    // pick the right image
-    images[currentSlide].style.zIndex = z
-    images[currentSlide].style.animation = "fade 0.5s"
-})
+  // pick the right image
+  images[currentSlide].style.zIndex = z;
+  images[currentSlide].style.animation = "fade 0.5s";
+});
 
 // when mouse goes over slide area, put all the images in a random place
-slideArea.addEventListener("mouseover", function () {
-    images.forEach(image => {
-        const x = 25 * (Math.floor(Math.random() * 5)) - 50
-        const y = 25 * (Math.floor(Math.random() * 5)) - 50
+slideArea.addEventListener("mouseover", function() {
+  images.forEach(image => {
+    const x = 25 * Math.floor(Math.random() * 5) - 50;
+    const y = 25 * Math.floor(Math.random() * 5) - 50;
 
-        image.style.transform = `translate(${x}px, ${y}px)`
-    })
-})
+    image.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
 
 // when mouse goes off, images reset
-slideArea.addEventListener("mouseout", function () {
-    images.forEach(image => {
-        image.style.transform = ""
-    })
-})
+slideArea.addEventListener("mouseout", function() {
+  images.forEach(image => {
+    image.style.transform = "";
+  });
+});
